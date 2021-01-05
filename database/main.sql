565febe585b9fafed982ce72f723fccfb9729307
abhirangana <abhishekrangana18@gmail.com>
2021-01-06 00:09:50 +0530

    modified the content in database content and verefied

--- database/main.sql
+++ database/main.sql
@@ -1,22 +1,5 @@
 CREATE DATABASE restaurant;
 USE restaurant;
-CREATE TABLE order(
-    id varchar(16) NOT NULL,
-    order_no NUMBER AUTO_INCREMENT,
-    table_no INT NOT NULL,
-    customer_name VARCHAR(20) NOT NULL,
-    customer_mobile VARCHAR(16) NOT NULL,
-    date DATETIME DEFAULT CURRENT_TIMESTAMP,
-    PRIMARY KEY(id)
-);
-CREATE TABLE order_item(
-    order_id VARCHAR(16) NOT NULL,
-    item_id VARCHAR(255) NOT NULL,
-    quantity INT DEFAULT 1,
-    PRIMARY KEY(order_id,item_id),
-    FOREIGN KEY(order_id) REFERENCES order(id),
-    FOREIGN KEY(item_id) REFERENCES item(id)
-);
 CREATE TABLE item (
     id VARCHAR(16) NOT NULL,
     name VARCHAR(255) NOT NULL,
@@ -32,5 +15,22 @@ CREATE TABLE item_feedback (
     feedback TEXT,
     PRIMARY KEY(id),
     CHECK(rating>=0 AND rating<=5),
+    FOREIGN KEY(item_id) REFERENCES item(id)
+);
+CREATE TABLE order_meta(
+    id VARCHAR(16) NOT NULL,
+    order_no INT,
+    table_no INT NOT NULL,
+    customer_name VARCHAR(20) NOT NULL,
+    customer_mobile VARCHAR(16) NOT NULL,
+    date DATETIME DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY(id)
+);
+CREATE TABLE order_item(
+    order_id VARCHAR(16) NOT NULL,
+    item_id VARCHAR(255) NOT NULL,
+    quantity INT DEFAULT 1,
+    PRIMARY KEY(order_id,item_id),
+    FOREIGN KEY(order_id) REFERENCES order_meta(id),
     FOREIGN KEY(item_id) REFERENCES item(id)
 );
