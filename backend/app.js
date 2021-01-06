da36c67f2e8ffbca46a3bf4c118d0f8d386a0876
CHILUKURI SRI HARSHA <sharshach@gmail.com>
2021-01-06 17:02:25 +0530

    Added views and some util

--- backend/app.js
+++ backend/app.js
@@ -3,7 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
+var ryfvhj = require('./util/random')
 // db connection SQL
 var mysql = require('mysql')
 var DBConnect = mysql.createConnection({
