2001413a8d294363ed5d2ac3bec6e213683f0515
abhirangana <abhishekrangana18@gmail.com>
2021-01-07 18:33:04 +0530

    added main.js for converting array list to sql list in util file

--- backend/util/main.js
+++ backend/util/main.js
@@ -1,9 +1,28 @@
-const arrayOfObjectsToSqlObjects= (arrayItems) =>{
+const arrayOfObjectsToSqlObjects= (dbConnect,arrayItems) =>{
     var result           = '';
-    arrayItems.foreach{ item->{
-        
-    }
-
-    }
+    var flag=0,mainflag=0;
+    arrayItems.forEach( item =>{
+        flag=0;
+        if(mainflag==0){
+            result+='(';
+            mainflag=1;
+        }
+        else{
+            result+=',(';
+        }
+        Object.keys(item).forEach( key=> {
+            if(flag==0){
+                result+=`${dbConnect.escape(item[key])}`;
+                flag=1;
+            }
+            else{
+                result+=`,${dbConnect.escape(item[key])}`;
+            }
+        });
+        result+=')';
+    })
     return result;
 };
+
+
+module.exports = {arrayOfObjectsToSqlObjects};
