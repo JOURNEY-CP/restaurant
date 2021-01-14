8359c134b1c678a5fa7a452cb7f175ae7ae0a6b5
VADDAVALLI SAI SITA RAM <saisitaramvaddavalli@gmail.com>
2021-01-14 19:29:55 +0530

    added basic components in frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
+import { BrowserRouter as Router,Switch,Route} from 'react-router-dom';
 import './App.css';
-
+import Header from './components/Header';
+import Footer from './components/Footer';
+import NotFound from './components/NotFound';
+import Home from './components/Home';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header/>
+      <Router>
+        <Switch>
+          <Route exact path={'/'} component={Home}/>
+          <Route path="*" component={NotFound} /> 
+        </Switch>
+      </Router>
+      <Footer/>
     </div>
   );
 }
