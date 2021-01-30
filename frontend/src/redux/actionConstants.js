12a9fce49a2b5d0954597ae954b53743835579de
CHILUKURI SRI HARSHA <sharshach@gmail.com>
2021-01-30 17:38:53 +0530

    add react

---
+++ frontend/src/redux/actionConstants.js
@@ -0,0 +1,4 @@
+export const MY_SAMPLE='mySample';
+export const MY_SAMPLE2='mySample2';
+
+export const GET_ITEM_LIST='getItemList';
---
+++ frontend/src/redux/actions/items.js
@@ -0,0 +1,8 @@
+import { GET_ITEM_LIST } from '../actionConstants';
+
+const onGetItemList = (data) => ({
+    type: GET_ITEM_LIST,
+    data
+});
+
+export { onGetItemList };
