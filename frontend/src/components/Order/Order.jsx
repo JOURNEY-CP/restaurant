cdfb78a0d14fda8118bd0e7e21a4eb93ea551b7d
CHILUKURI SRI HARSHA <sharshach@gmail.com>
2021-04-16 11:53:21 +0530

    Changed place order code from cart to orders

--- frontend/src/components/Order/Order.jsx
+++ frontend/src/components/Order/Order.jsx
@@ -1,6 +1,30 @@
 import React, { Component } from 'react'
 import './order.css';
+const axios = require('axios').default;
+
 class Order extends Component {
+
+    placeOrder = () => {
+        if (this.props.customerMetaData) {
+            axios
+                .post(`${process.env.REACT_APP_BACKEND_HOST}/api/user/order`, {
+                    customer_mobile: this.props.customerMetaData.mobile,
+                    customer_name: this.props.customerMetaData.name,
+                    table_no: this.props.customerMetaData.tableNo,
+                    item_list: this.props.order,
+                })
+                .then(function(response) {
+
+                    alert('Thanks for Ordering!!');
+                })
+                .catch(function(error) {
+                    console.log(error);
+                });
+        } else {
+            alert('something went wrong!');
+        }
+       
+    }
     render() {
         return (
             <div>
