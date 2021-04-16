16e0a1023baae7eb0e7997467eb2eace2ad99fa4
CHILUKURI SRI HARSHA <sharshach@gmail.com>
2021-04-16 11:39:27 +0530

    Added Order.jsx

---
+++ frontend/src/components/Order/Order.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import './order.css';
+class Order extends Component {
+    render() {
+        return (
+            <div>
+                Order in Progress
+            </div>
+        )
+    }
+}
+export default Order;
